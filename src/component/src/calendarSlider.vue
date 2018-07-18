3ac03281239e23992d8b1366e1e8b4f1b8fc2cb0
LinSky <397588184@qq.com>
2018-07-18 19:01:07 +0800

    get months

--- src/component/src/calendarSlider.vue
+++ src/component/src/calendarSlider.vue
@@ -1,23 +1,51 @@
 <template>
-    <div>
+    <div class="calendar_slider">
     </div>
 </template>
 
 <script>
+import moment from 'moment'
 export default {
     name: 'calendarSlider',
     props: {
+        defaultMonth: {
+            type: String,
+            default: moment().format('YYYY-MM')
+        }
     },
     data () {
         return {
         }
     },
-    computed: {},
+    computed: {
+        /**
+        *根据传入的defaultMonth,计算出上月，与下月
+        */
+        months () {
+            let vm = this
+            let monArr = []
+            monArr.push(
+                moment(vm.defaultMonth).subtract(1, 'M').format('YYYY-MM-DD'), //上一个月
+                moment(vm.defaultMonth).format('YYYY-MM-DD'),    //当月
+                moment(vm.defaultMonth).add(1, 'M').format('YYYY-MM-DD') //下一个月
+            )
+            return monArr
+        }
+    },
     watch: {
+
     },
     created () {
+        this.getWeekDates(this.months[1])
     },
     methods: {
+        getWeekDates (date) {
+            let vm = this,
+                calendars = [],
+                today = moment()
+            console.log(today.weeks());
+
+        }
     }
 }
 </script>
